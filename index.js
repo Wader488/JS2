601af3c0d82abbf31e300d7d60ddb8243058b82f
Wader488 <alexag488@gmail.com>
2019-12-17 19:39:15 +0300

    Задание с промис

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-//задание 1
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+function makeGETRequest(url) {
+    return new Promise((resolve, reject) => {
+        
+        let xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new window.XMLHttpRequest();
+    } else {
+        xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 ){
+            if (xhr.status === 200) {
+            const body = JSON.parse(xhr.responseText);
+            resolve(body);
+            } else {
+            reject(xhr.responseText);
+            }
+        
+        }
+    };
+        
+    xhr.onerror = function (error){
+        reject(error);
+    }
+        
+    xhr.open('GET', url);
+    xhr.send(); 
+    }
+    )  
+}
+/*    asyncFunc(5).then((res) => {
+        return asyncFunc(res + 1);
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.error(err);
+    });*/
+
+
+
 class cart {
-    cartDelivery(){
+    cartDelivery() {
         //расчет стоимости доставки
     }
-    cartSumm(){
+    cartSumm() {
         //Подсчет общей суммы товаров в корзине
     }
 }
 
-class cartProduct{
-    cartProductSumm(){
+class cartProduct {
+    cartProductSumm() {
         //Подсчет суммы товара каждого типа в корзине
     }
-    cartProductDiscount(){
+    cartProductDiscount() {
         //Подсчет скидки на товар каждого типа в корзине
     }
 }
@@ -33,55 +74,62 @@ class GoodsList {
         this.goods = [];
         this.totalSumm = 0;
     }
-    fetchGoods()  {
-        this.goods = [
-            { title: 'Shirt', price: 150 },
-            { title: 'Socks', price: 150 },
-            { title: 'Jacket', price: 150 },
-            { title: 'Shoes', price: 150 },
-            { title: 'Shirt red', price: 250 },
-            { title: 'Socks red', price: 250 },
-            { title: 'Jacket red', price: 250 },
-            { title: 'Shoes red', price: 250 },
-            { title: 'Shirt blue', price: 350 }
-        ];
+   /* fetchGoods(cb)  {
+        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = goods;
+            cb();
+        });
+    }*/
+    fetchGoods(cb)  {
+        makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+            this.goods = goods;
+            cb();
+        });
     }
+
+    totalPrice() {
+        return this.goods.reduce((accum, item) => {
+            if (item.price) accum += item.price;
+            return accum;
+        }, 0);
+    }
+
+
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price);
+            const goodItem = new GoodsItem(good.product_name, good.price);
             listHtml += goodItem.render();
         });
         document.querySelector('.goods-list').innerHTML = listHtml;
     }
-    
+
     //Задание 2: Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
     //Вариант 1
-    summGoods1(){
+    summGoods1() {
         let summ = 0;
         this.goods.forEach(good =>
             summ += good.price
         );
         console.log(summ);
     }
-    
+
     //Вариант 2, добавлено свойство totalSumm
-    summGoods2(){
-         this.goods.forEach(good => 
+    summGoods2() {
+        this.goods.forEach(good =>
             this.totalSumm += good.price
         );
     }
 }
 
 const list = new GoodsList();
-list.fetchGoods();
-list.render();
+list.fetchGoods(() => {
+    list.render();
+    
+    console.log('Вариант 1');
+    list.summGoods1();
 
-console.log('Вариант 1');
-list.summGoods1();
-
-console.log('Вариант 2');
-list.summGoods2();
-console.log(list.totalSumm);
-
-
+    console.log('Вариант 2');
+    list.summGoods2();
+    console.log(list.totalSumm);
+});
